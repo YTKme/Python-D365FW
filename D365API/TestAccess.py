421f1751f600a7a4a13c5e2eecf734f2643f8cdf
Yan Kuang <YTKme@Outlook.com>
2020-03-04 11:10:50 -0800

    Add TestAccess module

---
+++ D365API/TestAccess.py
@@ -0,0 +1,76 @@
+"""
+D365API.TestAccess
+~~~~~~~~~~~~~~~~~~
+"""
+
+import json
+import os
+import unittest
+from D365API.Access import Access
+
+class TestAccess(unittest.TestCase):
+    """Test the Access module."""
+
+    def setUp(self):
+        """Prepare test fixture.
+
+        Get the data from JSON (JavaScript Object Notation) file.
+        """
+
+        # Get the current directory of the file
+        current_directory = os.path.dirname(os.path.abspath(__file__))
+        # Get the path of the test data file
+        test_access_file = os.path.join(current_directory, "TestData.json")
+
+        # Open the file for reading
+        with open(test_access_file, "r") as f:
+            self._data = json.load(f)
+
+        # Get the hostname
+        self._hostname = self._data["organizations"]["name"]
+
+
+    def test_login_rest_v1_failure(self):
+        """Test a failure of REST (REpresentational State Transfer) login method.
+
+        Get the failure username and password (user_rest_failure) from the
+        TestAccess.json file and login. Should result in login method returning
+        None value.
+        """
+
+        # Get the user data for success login
+        user_rest_v1_failure = self._data["systemusers"]["user_rest_v1_failure"]
+
+        # Create an instance of Access object and login
+        access = Access(hostname=self._hostname,
+                        client_id=user_rest_v1_failure["client_id"],
+                        client_secret=user_rest_v1_failure["client_secret"],
+                        tenant_id=user_rest_v1_failure["tenant_id"]).login()
+
+        # Test to ensure access is not a string
+        self.assertNotEqual(type(access), str)
+
+
+    def test_login_rest_v1_success(self):
+        """Test a success of REST (REpresentational State Transfer) login method.
+
+        Get the success username and password (user_rest_v1_success) from the
+        TestAccess.json file and login. Should result in login method returning
+        an access token.
+        """
+
+        # Get the user data for success login
+        user_rest_v1_success = self._data["systemusers"]["user_rest_v1_success"]
+
+        # Create an instance of Access object and login
+        access = Access(hostname=self._hostname,
+                        client_id=user_rest_v1_success["client_id"],
+                        client_secret=user_rest_v1_success["client_secret"],
+                        tenant_id=user_rest_v1_success["tenant_id"]).login()
+
+        # Test to ensure access is a string
+        self.assertEqual(type(access), str)
+
+
+if __name__ == "__main__":
+    unittest.main()
