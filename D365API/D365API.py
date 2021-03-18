8d2463072433a3417af147c3c5950352d67a4c96
Yan Kuang <YTKme@Outlook.com>
2021-03-18 10:20:41 -0400

    Start main D365API module

--- D365API/D365API.py
+++ D365API/D365API.py
@@ -3,6 +3,8 @@ D365API
 ~~~~~~~
 """
 
+from D365API.Access import Access
+
 class D365API(object):
     """D365API Object
 
@@ -21,12 +23,18 @@ class D365API(object):
             tenant_id (str): The Directory (tenant) ID of the application.
         """
 
-        self._hostname = hostname
-        self._client_id = client_id
-        self._client_secret = client_secret
-        self._tenant_id = tenant_id
-        self._version = 1
-        self._rest_v1_url = "https://login.microsoftonline.com/{}/oauth2/token".format(self._tenant_id)
-        self._resource = "https://{}.api.crm.dynamics.com/".format(hostname)
-        self._rest_v2_url = "https://login.microsoftonline.com/{}/oauth2/v2.0/token".format(self._tenant_id)
-        self._scope = "https://{}.api.crm.dynamics.com/.default".format(hostname)
+        self.hostname = hostname
+        self.client_id = client_id
+        self.client_secret = client_secret
+        self.tenant_id = tenant_id
+        self.version = 1
+        self.rest_v1_url = f'https://login.microsoftonline.com/{self.tenant_id}/oauth2/token'
+        self.resource = f'https://{hostname}.api.crm.dynamics.com/'
+        self.rest_v2_url = f'https://login.microsoftonline.com/{self.tenant_id}/oauth2/v2.0/token'
+        self.scope = f'https://{hostname}.api.crm.dynamics.com/.default'
+
+        # Create an instance of Access object and login
+        access = Access(hostname=self.hostname,
+                        client_id=self.client_id,
+                        client_secret=self.client_secret,
+                        tenant_id=self.tenant_id).login()
