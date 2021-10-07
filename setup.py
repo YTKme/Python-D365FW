f31689ffbc425f33d8b6a4cc84f0c308d10f737b
Yan Kuang <YTKme@Outlook.com>
2021-10-07 14:57:41 -0700

    Add long description for setup

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 from setuptools import setup, find_packages
 
+# Read the contents of the README.md file
+from pathlib import Path
+current_directory = Path(__file__).parent
+long_description = (current_directory / 'README.md').read_text()
+
 setup(
     name = "d365api",
-    version = "0.7.2",
+    version = "0.7.3",
     packages = find_packages(),
 
     # Dependency
@@ -14,6 +19,8 @@ setup(
     author = "Yan Kuang",
     author_email = "YTKme@Outlook.com",
     description = "Microsoft Dynamics 365 Application Programming Interface.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     license = "GNU GENERAL PUBLIC LICENSE",
     keywords = "dynamics d365 api microsoft"
 )
